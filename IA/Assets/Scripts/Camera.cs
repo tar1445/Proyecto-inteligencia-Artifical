e351de7cbae98d3f54ef44667db9c203472bd803
tar1445 <rodrigueztomas423@gmail.com>
2026-04-16 14:51:01 -0300

    First person terminado

---
+++ IA/Assets/Scripts/Camera.cs
@@ -0,0 +1,34 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private float Sensibilidad = 100;
+    private float rotacionHorizontal = 0; 
+    private float rotacionVertical = 0;
+
+    [SerializeField] Transform player; 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float valorX = Input.GetAxis("Mouse X") * Sensibilidad * Time.deltaTime;
+        float valorY = Input.GetAxis("Mouse Y") * Sensibilidad * Time.deltaTime;
+
+        rotacionHorizontal += valorX;
+        rotacionVertical -= valorY;
+
+
+        rotacionVertical = math.clamp(rotacionVertical, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(rotacionVertical, 0f, 0f);
+
+        player.Rotate(Vector3.up * valorX);
+
+
+    }
+}
